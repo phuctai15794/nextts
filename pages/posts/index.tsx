99eee629c26d7462bab72ad2ca610ce53ef6fe90
phuctai15794@gmail.com <phuctai15794@gmail.com>
2022-07-30 12:25:42 +0700

    NextJs By Typescript

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 
-export interface PostListProps {}
-
-export default function PostList(props: PostListProps) {
-	return <div>Post list page</div>;
+export interface Post {
+	id: number;
+	title: string;
+	body: string;
 }
+
+export interface PostListProps {
+	posts: [];
+}
+
+export default function PostList({ posts }: PostListProps) {
+	return (
+		<>
+			<h1>Post list page</h1>
+			{posts && (
+				<ul>
+					{Array.isArray(posts) &&
+						posts.map((post: Post) => (
+							<li key={post.id}>
+								<h4 style={{ textTransform: 'uppercase', marginBottom: '0.25rem' }}>{post.title}</h4>
+								<p style={{ margin: '0px' }}>{post.body}</p>
+							</li>
+						))}
+				</ul>
+			)}
+		</>
+	);
+}
+
+export const getStaticProps: GetStaticProps<PostListProps> = async (context: GetStaticPropsContext) => {
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+	const data = await response.json();
+
+	return {
+		props: {
+			posts: data.map((item: Post) => ({
+				id: item.id,
+				title: item.title,
+				body: item.body,
+			})),
+		},
+	};
+};
